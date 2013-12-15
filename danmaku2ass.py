6c4811853500ac00ab10eae75f2cb1452e432589
Star Brilliant <m13253@hotmail.com>
2013-12-15 15:20:42 +0800

    Add declarations at the top of danmaku2ass.py

--- danmaku2ass.py
+++ danmaku2ass.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# The original author of this program, Danmaku2ASS, is StarBrilliant.
+# This file is released under General Public License version 3.
+# You should have received a copy of General Public License text alongside with
+# this program. If not, you can obtain it at http://gnu.org/copyleft/gpl.html .
+# This program comes with no warranty, the author will not be resopnsible for
+# any damage or problems caused by this program.
+
 import argparse
 import calendar
 import gettext
