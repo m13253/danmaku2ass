03e089ef4ab12e67b89dca27a544f363f7a5b33b
Star Brilliant <m13253@hotmail.com>
2013-12-20 20:52:08 +0800

    Add ScaleBorderAndShadow: yes to the resulting ASS file

--- danmaku2ass.py
+++ danmaku2ass.py
@@ -545,6 +545,7 @@ WrapStyle: 2
 Collisions: Normal
 PlayResX: %(width)s
 PlayResY: %(height)s
+ScaledBorderAndShadow: yes
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
